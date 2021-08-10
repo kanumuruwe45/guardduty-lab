0c73bff556436fe0a5cabf8843f9b514db3c8675
Kanumuru Rajesh <72786996+kanumuruwe45@users.noreply.github.com>
2021-08-10 15:59:08 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -12,9 +12,10 @@ def get_cmd():
     if request.method == "POST":
         if request.form.get("email"):
             url = request.form.get("email")
-            temp =  str(subprocess.call(["curl",url]))+ " has succesfully subscribed to AppSecEngineer"
-            print(temp)
-        return render_template("email.html",data=temp)
+#             temp =  str(subprocess.call(["curl",url]))+ " has succesfully subscribed to AppSecEngineer"
+            process = subprocess.Popen(['curl', url], stdout=subprocess.PIPE)
+            print(process)
+        return render_template("email.html",data=process)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
